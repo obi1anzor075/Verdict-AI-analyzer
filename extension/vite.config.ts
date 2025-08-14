2a47d8beae2d60362dceab59fb69c89f9eb4ed43
obi1anzor075 <shindin112@gmail.com>
2025-08-14 23:42:07 +0300

    Update vite.config.ts

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.config.js';
 
-// Если ты используешь TypeScript в модулях — добавь "ts" расширения
-// и можно указать alias для src, чтобы импортировать проще
 
 export default defineConfig({
   plugins: [crx({ manifest })],
@@ -13,7 +11,6 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        // CRXJS сам найдёт entry points из manifest, но можно явно прописать
         background: 'src/background/background.js',
         content: 'src/content/content.init.js',
       },
@@ -24,7 +21,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src', // позволяет писать import from '@/content/utils'
+      '@': '/src', /
     },
   },
 });
